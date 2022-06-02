c4118e109fa58adf6bc860a5202cbc652935ef09
ricky <zhangqingcq@foxmail.com>
2022-06-02 08:45:30 +0800

    add map files

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  china: require('china-map-data/china'),
+  province: require('china-map-data/province'),
+  'china-contour': require('china-map-data/china-contour'),
+  world: require('china-map-data/world')
+}
